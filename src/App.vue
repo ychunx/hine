57f05de010d7e0eaded052ba28ab9c7f959bfb3a
ychunx <ychunx@qq.com>
2023-01-03 09:54:38 +0800

    禁止长按选择

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,28 @@ export default {
 *{
     padding: 0;
     margin: 0;
+
+    // 禁止长按选择
+    -webkit-touch-callout: none;
+    /*系统默认菜单被禁用*/
+    -webkit-user-select: none;
+    /*webkit浏览器*/
+    -khtml-user-select: none;
+    /*早期浏览器*/
+    -moz-user-select: none;
+    /*火狐*/
+    -ms-user-select: none;
+    /*IE10*/
+    user-select: none;
+  }
+  // 恢复输入框
+  input {
+    -webkit-user-select: auto;
+    /*webkit浏览器*/
+  }
+  textarea {
+    -webkit-user-select: auto;
+    /*webkit浏览器*/
   }
   #app {
     width: 100%;
